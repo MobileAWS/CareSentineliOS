035b51d30d40b1311c813bf05c46fb3b90ef2d53
Carlos Alvarez <mike@mobileaws.com>
2016-01-14 17:42:45 -0500

    no message

--- CareSentineliOS/Controllers/SmsViewController.h
+++ CareSentineliOS/Controllers/SmsViewController.h
@@ -11,7 +11,7 @@
 #import <AddressBookUI/AddressBookUI.h>
 
 @interface SmsViewController : UITableViewController
-@property (strong, nonatomic) IBOutlet UIButton *logoutButton;
+@property (weak, nonatomic) IBOutlet UIButton *logoutButton;
 - (IBAction)logoutAction:(id)sender;
 - (Boolean) findContactExist:(NSArray*)contactList contact:(NSString*) newContactName;
 @end
